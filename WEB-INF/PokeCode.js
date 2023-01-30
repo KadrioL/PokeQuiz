c81984cb17f0e9a4aca547ed8844d53e46523e71
KadrioL <adrian.69518@web.de>
2023-01-30 13:59:08 +0100

    first version

---
+++ WEB-INF/PokeCode.js
@@ -0,0 +1,373 @@
+var pokemon1 = { td1: "bisasam", td2: "bisaknosp", td3: "bisaflor", td4: "glumanda", td5: "glutexo", td6: "glurak", td7: "schiggy", td8: "schillok", td9: "turtok", td10: "raupy", td11: "safcon", td12: "smettbo", td13: "hornliu", td14: "kokuna", td15: "bibor", td16: "taubsi", td17: "tauboga", td18: "tauboss", td19: "rattfratz", td20: "rattikarl", td21: "habitak", td22: "ibitak", td23: "rettan", td24: "arbok", td25: "pikachu", td26: "raichu", td27: "sandan", td28: "sandamer", td29: "nidoran", td30: "nidorina", td31: "nidoqueen", td32: "nidoran", td33: "nidorino", td34: "nidoking", td35: "piepi", td36: "pixi", td37: "vulpix", td38: "vulnona", td39: "pummeluff", td40: "knuddeluff", td41: "zubat", td42: "golbat", td43: "myrapla", td44: "duflor", td45: "giflor", td46: "paras", td47: "parasek", td48: "bluzuk", td49: "omot", td50: "digda", td51: "digdri", td52: "mauzi", td53: "snobilikat", td54: "enton", td55: "entoron", td56: "menki", td57: "rasaff", td58: "fukano", td59: "arkani", td60: "quapsel", td61: "quaputzi", td62: "quappo", td63: "abra", td64: "kadabra", td65: "simsala", td66: "machollo", td67: "maschock", td68: "machomei", td69: "knofensa", td70: "ultrigaria", td71: "sarzenia", td72: "tentacha", td73: "tentoxa", td74: "kleinstein", td75: "georok", td76: "geowaz", td77: "ponita", td78: "gallopa", td79: "flegmon", td80: "lahmus", td81: "magnetilo", td82: "magneton", td83: "porenta", td84: "dodu", td85: "dodri", td86: "jurob", td87: "jugong", td88: "sleima", td89: "sleimok", td90: "muschas", td91: "austos", td92: "nebulak", td93: "alpollo", td94: "gengar", td95: "onix", td96: "traumato", td97: "hypno", td98: "krabby", td99: "kingler", td100: "voltobal", td101: "lektrobal", td102: "owei", td103: "kokowei", td104: "tragosso", td105: "knogga", td106: "kicklee", td107: "nockchan", td108: "schlurp", td109: "smogon", td110: "smogmog", td111: "rihorn", td112: "rizeros", td113: "chaneira", td114: "tangela", td115: "kangama", td116: "seeper", td117: "seemon", td118: "goldini", td119: "golking", td120: "sterndu", td121: "starmie", td122: "pantimos", td123: "sichlor", td124: "rossana", td125: "elektek", td126: "magmar", td127: "pinsir", td128: "tauros", td129: "karpador", td130: "garados", td131: "lapras", td132: "ditto", td133: "evoli", td134: "aquana", td135: "blitza", td136: "flamara", td137: "porygon", td138: "amonitas", td139: "amoroso", td140: "kabuto", td141: "kabutops", td142: "aerodactyl", td143: "relaxo", td144: "arktos", td145: "zapdos", td146: "lavados", td147: "dratini", td148: "dragonir", td149: "dragoran", td150: "mewtu", td151: "mew" };
+var pokemon2 = { td1: "endivie", td2: "lorblatt", td3: "meganie", td4: "feurigel", td5: "igelavar", td6: "tornupto", td7: "karnimani", td8: "tyracroc", td9: "impergator", td10: "wiesor", td11: "wiesenior", td12: "hoothoot", td13: "noctuh", td14: "ledyba", td15: "ledian", td16: "webarak", td17: "ariados", td18: "iksbat", td19: "lampi", td20: "lanturn", td21: "pichu", td22: "pii", td23: "fluffeluff", td24: "togepi", td25: "togetic", td26: "natu", td27: "xatu", td28: "voltilamm", td29: "waaty", td30: "ampharos", td31: "blubella", td32: "marill", td33: "azumarill", td34: "mogelbaum", td35: "quaxo", td36: "hoppspross", td37: "hubelupf", td38: "papungha", td39: "griffel", td40: "sonnkern", td41: "sonnflora", td42: "yanma", td43: "felino", td44: "morlord", td45: "psiana", td46: "nachtara", td47: "kramurx", td48: "laschoking", td49: "traunfugil", td50: "icognito", td51: "woingenau", td52: "girafarig", td53: "tannza", td54: "forstellka", td55: "dummisel", td56: "skorgla", td57: "stahlos", td58: "snubbull", td59: "granbull", td60: "baldorfish", td61: "scherox", td62: "pottrott", td63: "skaraborn", td64: "sniebel", td65: "teddiursa", td66: "ursaring", td67: "schneckmag", td68: "magcargo", td69: "quiekel", td70: "keifel", td71: "corasonn", td72: "remoraid", td73: "octillery", td74: "botogel", td75: "mantax", td76: "panzaeron", td77: "hunduster", td78: "hundemon", td79: "seedraking", td80: "phanpy", td81: "donphan", td82: "porygon-2", td83: "damhirplex", td84: "farbeagle", td85: "rabauz", td86: "kapoera", td87: "kussilla", td88: "elekid", td89: "magby", td90: "miltank", td91: "heiteira", td92: "raikou", td93: "entei", td94: "suicune", td95: "larvitar", td96: "pupitar", td97: "despotar", td98: "lugia", td99: "ho-oh", td100: "celebi" };
+var pokemon3 = { td1: "geckarbor", td2: "reptain", td3: "gewaldro", td4: "flemmli", td5: "jungglut", td6: "lohgock", td7: "hydropi", td8: "moorabbel", td9: "sumpex", td10: "fiffyen", td11: "magnayen", td12: "zigzachs", td13: "geradaks", td14: "waumpel", td15: "schaloko", td16: "papinella", td17: "panekon", td18: "pudox", td19: "loturzel", td20: "lombrero", td21: "kappalores", td22: "samurzel", td23: "blanas", td24: "tengulist", td25: "schwalbini", td26: "schwalboss", td27: "wingull", td28: "pelipper", td29: "trasla", td30: "kirlia", td31: "guardevoir", td32: "geweiher", td33: "maskeregen", td34: "knilz", td35: "kapilz", td36: "bummelz", td37: "muntier", td38: "letarking", td39: "nincada", td40: "ninjask", td41: "ninjatom", td42: "flurmel", td43: "krakeelo", td44: "krawumms", td45: "makuhita", td46: "hariyama", td47: "azurill", td48: "nasgnet", td49: "eneco", td50: "enekoro", td51: "zobiris", td52: "flunkifer", td53: "stollunior", td54: "stollrak", td55: "stolloss", td56: "meditie", td57: "meditalis", td58: "frizelbliz", td59: "voltenso", td60: "plusle", td61: "minun", td62: "volbeat", td63: "illumise", td64: "roselia", td65: "schluppuck", td66: "schlukwech", td67: "kanivanha", td68: "tohaido", td69: "wailmer", td70: "wailord", td71: "camaub", td72: "camerupt", td73: "qurtel", td74: "spoink", td75: "groink", td76: "pandir", td77: "knacklion", td78: "vibrava", td79: "libelldra", td80: "tuska", td81: "noktuska", td82: "wablu", td83: "altaria", td84: "sengo", td85: "vipitis", td86: "lunastein", td87: "sonnfel", td88: "schmerbe", td89: "welsar", td90: "krebscorps", td91: "krebutack", td92: "puppance", td93: "lepumentas", td94: "liliep", td95: "wielie", td96: "anorith", td97: "armaldo", td98: "barschwa", td99: "milotic", td100: "formeo", td101: "kecleon", td102: "shuppet", td103: "banette", td104: "zwirrlicht", td105: "zwirrklop", td106: "tropius", td107: "palimpalim", td108: "absol", td109: "isso", td110: "schneppke", td111: "firnontor", td112: "seemops", td113: "seejong", td114: "walraisa", td115: "perlu", td116: "aalabyss", td117: "saganabyss", td118: "relicanth", td119: "liebiskus", td120: "kindwurm", td121: "draschel", td122: "brutalanda", td123: "tanhel", td124: "metang", td125: "metagross", td126: "regirock", td127: "regice", td128: "registeel", td129: "latias", td130: "latios", td131: "kyogre", td132: "groudon", td133: "rayquaza", td134: "jirachi", td135: "deoxys" };
+var pokemon4 = { td1: "chelast", td2: "chelcarain", td3: "chelterrar", td4: "panflam", td5: "panpyro", td6: "panferno", td7: "plinfa", td8: "pliprin", td9: "impoleon", td10: "staralili", td11: "staravia", td12: "staraptor", td13: "bidiza", td14: "bidifas", td15: "zirpurze", td16: "zirpeise", td17: "sheinux", td18: "luxio", td19: "luxtra", td20: "knospi", td21: "roserade", td22: "koknodon", td23: "rameidon", td24: "schilterus", td25: "bollterus", td26: "burmy", td27: "burmadame", td28: "moterpel", td29: "wadribie", td30: "honweisel", td31: "pachirisu", td32: "bamelin", td33: "bojelin", td34: "kikugi", td35: "kinoso", td36: "schalellos", td37: "gastrodon", td38: "ambidiffel", td39: "driftlon", td40: "drizepeli", td41: "haspiror", td42: "schlapor", td43: "traunmagil", td44: "kramshef", td45: "charmian", td46: "shnurgarst", td47: "klingplim", td48: "skunkapuh", td49: "skuntank", td50: "bronzel", td51: "bronzong", td52: "mobai", td53: "pantimimi", td54: "wonneira", td55: "plaudagei", td56: "kryppuk", td57: "kaumalat", td58: "knarksel", td59: "knakrack", td60: "mampfaxo", td61: "riolu", td62: "lucario", td63: "hippopotas", td64: "hippoterus", td65: "pionskora", td66: "piondragi", td67: "glibunkel", td68: "toxiquak", td69: "venuflibis", td70: "finneon", td71: "lumineon", td72: "mantirps", td73: "shnebedeck", td74: "rexblisar", td75: "snibunna", td76: "magnezone", td77: "schlurplek", td78: "rihornior", td79: "tangoloss", td80: "elevoltek", td81: "magbrant", td82: "togekiss", td83: "yanmega", td84: "folipurba", td85: "glaziola", td86: "skorgro", td87: "mamutel", td88: "porygon-z", td89: "galagladi", td90: "voluminas", td91: "zwirrfinst", td92: "frosdedje", td93: "rotom", td94: "selfe", td95: "vesprit", td96: "tobutz", td97: "dialga", td98: "palkia", td99: "heatran", td100: "regigigas", td101: "giratina", td102: "cresselia", td103: "phione", td104: "manaphy", td105: "darkrai", td106: "shaymin", td107: "arceus" };
+let zaehler = 0;
+let n;
+let pokeCounter = 0;
+var checkList = new Array();
+var surrendered = false;
+var countDownDate
+
+document.addEventListener('DOMContentLoaded', function() {
+	const buttonElement = document.getElementById("surrender");
+	buttonElement.addEventListener("click", surrender);
+}, false);
+
+
+
+
+function surrender() {
+
+	if (n == 1) {
+		document.getElementById("userInput").setAttribute("disabled", "disabled");
+		document.querySelector('#surrender').disabled = true;
+		for (var key in pokemon1) {
+			if (!checkList.includes(pokemon1[key])) {
+				let cell = document.getElementById("" + key);
+				cell.setAttribute("style", "color:red;");
+				let text = document.createTextNode("" + capitalizeFirstLetter(pokemon1[key]))
+				cell.appendChild(text);
+			}
+		}
+		surrendered = true;
+		return surrendered;
+	} else if (n == 2) {
+		document.getElementById("userInput").setAttribute("disabled", "disabled");
+		document.querySelector('#surrender').disabled = true;
+		for (var key in pokemon2) {
+			if (!checkList.includes(pokemon2[key])) {
+				let cell = document.getElementById("" + key);
+				cell.setAttribute("style", "color:red;");
+				let text = document.createTextNode("" + capitalizeFirstLetter(pokemon2[key]))
+				cell.appendChild(text);
+			}
+		}
+		surrendered = true;
+		return surrendered;
+	} else if (n == 3) {
+		document.getElementById("userInput").setAttribute("disabled", "disabled");
+		document.querySelector('#surrender').disabled = true;
+		for (var key in pokemon3) {
+			if (!checkList.includes(pokemon3[key])) {
+				let cell = document.getElementById("" + key);
+				cell.setAttribute("style", "color:red;");
+				let text = document.createTextNode("" + capitalizeFirstLetter(pokemon3[key]))
+				cell.appendChild(text);
+			}
+		}
+		surrendered = true;
+		return surrendered;
+	} else if (n == 4) {
+		document.getElementById("userInput").setAttribute("disabled", "disabled");
+		document.querySelector('#surrender').disabled = true;
+		for (var key in pokemon4) {
+			if (!checkList.includes(pokemon4[key])) {
+				let cell = document.getElementById("" + key);
+				cell.setAttribute("style", "color:red;");
+				let text = document.createTextNode("" + capitalizeFirstLetter(pokemon4[key]))
+				cell.appendChild(text);
+			}
+		}
+		surrendered = true;
+		return surrendered;
+	}
+
+
+}
+
+function stopTimer() {
+	if (document.getElementById("timer") != "") {
+		document.getElementById("timer").innerHTML = "";
+	}
+
+
+}
+function startTimer() {
+	surrendered = false;
+	stopTimer();
+	countDownDate = new Date().getTime() + 15.01 * 60 * 1000;
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+
+		// Get today's date and time
+		var now = new Date().getTime();
+
+		// Find the distance between now and the count down date
+		var distance = countDownDate - now;
+
+		// Time calculations for hours, minutes and seconds
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		seconds = seconds <= 9 ? '0' + seconds : seconds;
+		// Display the result in the element with id="demo"
+		document.getElementById("timer").innerHTML = minutes + ":" + seconds + " || Gen " + n;
+
+		// If the count down is finished, write some text
+		if (distance < 0 || surrendered) {
+			clearInterval(x);
+			document.getElementById("timer").innerHTML = "00:00 || Gen " + n;
+			document.getElementById("userInput").setAttribute("disabled", "disabled");
+		}
+	}, 1000);
+}
+function generateTable() {
+	document.querySelector('#surrender').disabled = false;
+	pokeCounter = 0;
+	if (zaehler != 0) {
+		document.getElementById("pokeTable").remove();
+	}
+	const tbl = document.createElement("table");
+	const tblBody = document.createElement("tbody");
+
+	n = document.getElementById("gens").value;
+	let counter = 1;
+	if (n == 1) {
+
+		for (let i = 0; i < 51; i++) {
+			// creates a table row
+			const row = document.createElement("tr");
+
+			for (let j = 0; j < 3; j++) {
+
+				// Create a <td> element and a text node, make the text
+				// node the contents of the <td>, and put the <td> at
+				// the end of the table row
+				if (counter <= 151) {
+					const cell = document.createElement("td");
+					cell.setAttribute("id", "td" + counter)
+					const cellText = document.createTextNode("" + counter + ": ");
+					cell.appendChild(cellText);
+					row.appendChild(cell);
+					counter++;
+				}
+
+			}
+
+			// add the row to the end of the table body
+			tblBody.appendChild(row);
+			document.getElementById("counter").innerText = "" + pokeCounter + "/151 ||";
+		}
+	} else if (n == 2) {
+		for (let i = 0; i < 34; i++) {
+			// creates a table row
+			const row = document.createElement("tr");
+
+			for (let j = 0; j < 3; j++) {
+
+				// Create a <td> element and a text node, make the text
+				// node the contents of the <td>, and put the <td> at
+				// the end of the table row
+				if (counter <= 100) {
+					const cell = document.createElement("td");
+					cell.setAttribute("id", "td" + counter)
+					const cellText = document.createTextNode("" + counter + ": ");
+					cell.appendChild(cellText);
+					row.appendChild(cell);
+					counter++;
+				}
+
+			}
+
+			// add the row to the end of the table body
+			tblBody.appendChild(row);
+			document.getElementById("counter").innerText = "" + pokeCounter + "/100 ||";
+		}
+	} else if (n == 3) {
+		for (let i = 0; i < 45; i++) {
+			// creates a table row
+			const row = document.createElement("tr");
+
+			for (let j = 0; j < 3; j++) {
+
+				// Create a <td> element and a text node, make the text
+				// node the contents of the <td>, and put the <td> at
+				// the end of the table row
+				if (counter <= 135) {
+					const cell = document.createElement("td");
+					cell.setAttribute("id", "td" + counter)
+					const cellText = document.createTextNode("" + counter + ": ");
+					cell.appendChild(cellText);
+					row.appendChild(cell);
+					counter++;
+				}
+
+			}
+
+			// add the row to the end of the table body
+			tblBody.appendChild(row);
+			document.getElementById("counter").innerText = "" + pokeCounter + "/135 ||";
+		}
+	} else if (n == 4) {
+		for (let i = 0; i < 36; i++) {
+			// creates a table row
+			const row = document.createElement("tr");
+
+			for (let j = 0; j < 3; j++) {
+
+				// Create a <td> element and a text node, make the text
+				// node the contents of the <td>, and put the <td> at
+				// the end of the table row
+				if (counter <= 107) {
+					const cell = document.createElement("td");
+					cell.setAttribute("id", "td" + counter)
+					const cellText = document.createTextNode("" + counter + ": ");
+					cell.appendChild(cellText);
+					row.appendChild(cell);
+					counter++;
+				}
+
+			}
+
+			// add the row to the end of the table body
+			tblBody.appendChild(row);
+			document.getElementById("counter").innerText = "" + pokeCounter + "/107 ||";
+		}
+	}
+
+	// put the <tbody> in the <table>
+	tbl.appendChild(tblBody);
+	// appends <table> into <body>
+	document.body.appendChild(tbl);
+	// sets the border attribute of tbl to '2'
+	tbl.setAttribute("id", "pokeTable");
+	tbl.setAttribute("class", "fl-table");
+	zaehler++;
+	startTimer();
+}
+
+function checkInputGen() {
+	if (n == 1) {
+		checkInput(1);
+	} else if (n == 2) {
+		checkInput(2);
+	} else if (n == 3) {
+		checkInput(3);
+	} else if (n == 4) {
+		checkInput(4);
+	}
+}
+
+function checkInput(m) {
+	let input = document.getElementById("userInput").value;
+	input = lowerInput(input);
+
+	if (m == 1) {
+		for (var key in pokemon1) {
+			if (input == pokemon1[key] && !checkList.includes(input)) {
+				checkList.push(pokemon1[key]);
+				pokeCounter++;
+
+				let cell = document.getElementById("" + key);
+				if (lowerInput(input) == "nidoran") {
+					let cell2 = document.getElementById("td32");
+					cell.appendChild(document.createTextNode("" + capitalizeFirstLetter(input) + "(w)"));
+					cell2.appendChild(document.createTextNode("" + capitalizeFirstLetter(input) + "(m)"));
+				} else {
+					input = capitalizeFirstLetter(input);
+					cell.appendChild(document.createTextNode("" + input));
+
+				}
+
+
+				document.getElementById("userInput").value = "";
+				var element;
+
+				element = document.getElementById("counter");
+				if (element) {
+					if (lowerInput(input) == "nidoran") {
+						pokeCounter++;
+						element.innerHTML = "" + pokeCounter + "/151  ||";
+					} else {
+						element.innerHTML = "" + pokeCounter + "/151  ||";
+					}
+
+				}
+
+			}
+
+		}
+		if (checkList.length == 150) {
+			window.alert("Hurra du hast alle Pokemon der ersten Generation erraten")
+		}
+	} else if (m == 2) {
+		for (var key in pokemon2) {
+			if (input == pokemon2[key] && !checkList.includes(input)) {
+				checkList.push(pokemon2[key]);
+				pokeCounter++;
+
+				let cell = document.getElementById("" + key);
+
+				input = capitalizeFirstLetter(input);
+				cell.appendChild(document.createTextNode("" + input));
+
+
+
+
+				document.getElementById("userInput").value = "";
+				var element;
+
+				element = document.getElementById("counter");
+				if (element) {
+					element.innerHTML = "" + pokeCounter + "/151  ||";
+
+				}
+
+			}
+
+		}
+		if (checkList.length == 99) {
+			window.alert("Hurra du hast alle Pokemon der zweiten Generation erraten")
+		}
+	} else if (m == 3) {
+		for (var key in pokemon3) {
+			if (input == pokemon3[key] && !checkList.includes(input)) {
+				checkList.push(pokemon3[key]);
+				pokeCounter++;
+
+				let cell = document.getElementById("" + key);
+
+				input = capitalizeFirstLetter(input);
+				cell.appendChild(document.createTextNode("" + input));
+
+
+
+
+				document.getElementById("userInput").value = "";
+				var element;
+
+				element = document.getElementById("counter");
+				if (element) {
+					element.innerHTML = "" + pokeCounter + "/151  ||";
+
+				}
+
+			}
+
+		}
+		if (checkList.length == 134) {
+			window.alert("Hurra du hast alle Pokemon der dritten Generation erraten")
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function lowerInput(string) {
+	return string.toLowerCase();
+}
